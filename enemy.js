01e5880c7895b167aeced0608d71a2b305efdf32
Kevin Bedi <kevin@kevinbedi.com>
2014-01-07 00:43:15 -0500

    Initial commit

---
+++ enemy.js
@@ -0,0 +1,96 @@
+(function() {
+    window.enemies = {};
+    var _id = 0;
+
+    var Enemy = function(position) {
+        console.log(Object.keys(window.enemies).length);
+
+        var geometry = new THREE.CubeGeometry(0.5, 0.5, 1);
+        var material = new THREE.MeshBasicMaterial({color: 0x2222cc});
+        var enemy = new THREE.Mesh(geometry, material);
+        window.scene.add(enemy);
+
+        this.object = enemy;
+        this.health = 1;
+
+        this.speed = Math.random() * 0.12;
+
+        if (this.speed < 0.04)
+            this.speed = 0.04;
+
+        // Find random spawn location that is not too near to the player
+        var spawnPoint;
+        while (true) {
+            spawnPoint = new THREE.Vector3(Math.random() * 20 - 10, Math.random() * 20 - 10, 0);
+            if ((spawnPoint.x > window.player.position.x + 1 || spawnPoint.x < window.player.position.x - 1)
+                && (spawnPoint.y > window.player.position.y + 1 || spawnPoint.y < window.player.position.y - 1)) {
+                break;
+            }
+        }
+        this.object.position = spawnPoint;
+
+        this.id = _id++;
+        window.enemies[this.id] = this;
+    };
+
+    Enemy.prototype.logic = function(deltaTime) {
+        var playerPos = new THREE.Vector3(window.player.position.x, window.player.position.y, 0);
+        var direction = playerPos.sub(this.object.position).normalize().multiplyScalar(this.speed * deltaTime / 17);
+
+        var collidedEnemy = this.collidesWithAnyEnemy();
+        if (collidedEnemy) {
+            var yourPos = new THREE.Vector3(this.object.position.x, this.object.position.y);
+            var fixDir = yourPos.sub(collidedEnemy.object.position);
+            this.object.position.add(fixDir.multiplyScalar(0.1));
+        }
+        this.object.position.add(direction);
+        this.direction = direction.normalize();
+
+        if (this.collidesWith(window.player)) {
+            // document.body.innerHTML = "Game Over!";
+        }
+    }
+
+    Enemy.prototype.collidesWithAnyEnemy = function() {
+        for (var id in window.enemies) {
+            var enemy = window.enemies[id];
+
+            if (id != this.id && this.collidesWithEnemy(enemy)) {
+                return enemy;
+            }
+        }
+        return false;
+    }
+
+    Enemy.prototype.collidesWithEnemy = function(enemy) {
+        return this.collides(enemy.object.position.x, enemy.object.position.y, 0.5, 0.5);
+    };
+
+    Enemy.prototype.collidesWith = function(player) {
+        return this.collides(player.position.x, player.position.y, 1, 1);
+    };
+
+    Enemy.prototype.collides = function(x, y, w, h) {
+        var halfW = w / 2;
+        var halfH = h / 2;
+
+        var halfOurW = 0.25;
+        var halfOurH = 0.25;
+
+        if (this.object.position.x + halfOurW > x - halfW && this.object.position.x - halfOurW < x + halfW && this.object.position.y + halfOurH > y - halfH && this.object.position.y - halfOurH < y + halfH) {
+            return true;
+        }
+
+        return false;
+    };
+
+    Enemy.prototype.damage = function() {
+        this.health -= 1;
+        if (this.health <= 0) {
+            scene.remove(this.object);
+            delete window.enemies[this.id];
+        }
+    }
+
+    window.Enemy = Enemy;
+})();
---
+++ particle.js
@@ -0,0 +1,123 @@
+(function() {
+    var _id = 0;
+
+    var Particle = function(args) {
+        if (Object.keys(window.particles).length > 100) {
+            return;
+        }
+
+        this.position = args.position;
+        this.direction = args.direction;
+        this.lifetime = args.lifetime || -1;
+        this.speed = args.speed || 0.2;
+        this.mass = (args.mass !== undefined) ? args.mass : 1;
+
+        this.startColor = args.startColor || Math.round(Math.random() * 16777215);
+        this.endColor = args.endColor || this.startColor;
+
+        var geometry = new THREE.CubeGeometry(0.2 * this.mass || 0.08, 0.03 * this.mass || 0.012, 0.1 * this.mass || 0.08);
+        var material = new THREE.MeshBasicMaterial({color: this.startColor, transparent: true});
+        var particle = new THREE.Mesh(geometry, material);
+        particle.position = args.position;
+        if (args.direction.y < 0)
+            particle.rotation = new THREE.Euler(0, 0, 2 * Math.PI - args.direction.angleTo(new THREE.Vector3(1, 0, 0)));
+        else
+            particle.rotation = new THREE.Euler(0, 0, args.direction.angleTo(new THREE.Vector3(1, 0, 0)));
+        window.scene.add(particle);
+
+        this.object = particle;
+
+        this.id = _id++;
+        this.timeCreated = Date.now();
+        window.particles[this.id] = this;
+    };
+
+    Particle.prototype.project = function(deltaTime) {
+        var direction = new THREE.Vector3(this.direction.x, this.direction.y, this.direction.z);
+        this.object.position.add(direction.multiplyScalar(this.speed * deltaTime / 17));
+
+        if (this.object.position.x > 10 || this.object.position.y > 10 || this.object.position.x < -10 || this.object.position.y < -10) {
+            this._destroy(true);
+            return;
+        }
+
+        if (this.lifetime > 0) {
+            var timeToDie = this.lifetime - Date.now() + this.timeCreated;
+            var lifePercent = timeToDie / this.lifetime;
+            if (lifePercent < 0)
+                lifePercent = 0;
+            this.object.material.opacity = lifePercent;
+            if (this.endColor != this.startColor) {
+                var red = this.startColor.r + (this.endColor.r - this.startColor.r) * Math.sqrt(1-lifePercent);
+                var green = this.startColor.g + (this.endColor.g - this.startColor.g) * Math.sqrt(1-lifePercent);
+                var blue = this.startColor.b + (this.endColor.b - this.startColor.b) * Math.sqrt(1-lifePercent);
+
+                console.log(red, green, blue);
+                this.object.material.color.setRGB(red, green, blue);
+            }
+            if (Date.now() - this.timeCreated > this.lifetime) {
+                this._destroy(false);
+                return;
+            }
+        }
+
+        // Check for collision
+        for (var id in window.enemies) {
+            var enemy = window.enemies[id];
+
+            if (this.lifetime < 0 && this.collidesWith(enemy)) {
+                enemy.damage();
+                this._destroy(true);
+                return;
+            }
+        }
+    };
+
+    Particle.prototype.collidesWith = function(enemy) {
+        return this.collides(enemy.object.position.x, enemy.object.position.y, 1, 1);
+    };
+
+    Particle.prototype.collides = function(x, y, w, h) {
+        var halfW = w / 2;
+        var halfH = h / 2;
+        if (this.object.position.x > x - halfW && this.object.position.x < x + halfW && this.object.position.y > y - halfH && this.object.position.y < y + halfH) {
+            return true;
+        }
+
+        return false;
+    };
+
+    Particle.prototype._destroy = function(collided) {
+        scene.remove(this.object);
+        delete window.particles[this.id];
+
+        if (this.mass > 0 && collided && this.object.material.opacity > 0.1) {
+            var numParticles = this.mass * 10;
+            var lifetime = this.mass * 400;
+            for (var theta = 0; theta <= 2 * Math.PI; theta += 2 * Math.PI / numParticles) {
+                (function(particle, theta) {
+                    setTimeout(function() {
+                        var speed = particle.mass * 0.1 * Math.random();
+                        theta = Math.round(theta * 100) / 100;
+                        y = Math.sin(theta)
+                        x = Math.cos(theta);
+
+                        var position = new THREE.Vector3(particle.object.position.x, particle.object.position.y, particle.object.position.z);
+
+                        new Particle({
+                            position: position,
+                            direction: new THREE.Vector3(x, y, 0).normalize(),
+                            mass: particle.mass - 1,
+                            lifetime: lifetime,
+                            speed: speed,
+                            startColor: new THREE.Color(0xff0000),
+                            endColor: new THREE.Color(0xb8860b)
+                        });
+                    }, 1);
+                })(this, theta);
+            }
+        }
+    };
+
+    window.Particle = Particle;
+})();
